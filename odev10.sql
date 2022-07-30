ad9cbc1ff24ccddac2ffea940173159257bed916
Caradloki <30695845+Caradloki@users.noreply.github.com>
2022-07-30 16:24:48 +0300

    Update odev10.sql

--- odev10.sql
+++ odev10.sql
@@ -1,16 +1,16 @@
 -- Ödev 10
 
 -- Soru 1
-SELECT city.city, country.country from city
-left join country
-on city.country_id = country.country_id
+SELECT city.city, country.country FROM city
+LEFT JOIN country
+ON city.country_id = country.country_id
 
 -- Soru 2
-select customer.first_name, customer.last_name,payment.payment_id from payment
-right join customer
-on payment.customer_id = customer.customer_id
+SELECT customer.first_name, customer.last_name,payment.payment_id FROM payment
+RIGTH JOIN customer
+ON payment.customer_id = customer.customer_id
 
 -- Soru 3
-select rental.rental_id, customer.first_name,customer.last_name from customer
-full join rental
-on customer.customer_id = rental.customer_id
+SELECT rental.rental_id, customer.first_name,customer.last_name FROM customer
+FULL JOIN rental
+ON customer.customer_id = rental.customer_id
